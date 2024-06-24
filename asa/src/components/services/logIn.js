303e7cba274d205ff32fc880f282e2d80851af96
PrimeRin <primedruk312@gmail.com>
2024-06-24 16:06:06 +0600

    update backend url

--- asa/src/components/services/logIn.js
+++ asa/src/components/services/logIn.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_BACKEND_URL;
+const API_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 const login = async (username, password) => {
   try {
