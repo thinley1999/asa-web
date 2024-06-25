b70fa8187d87abcb6208329277a6f70c98b1735b
PrimeRin <primedruk312@gmail.com>
2024-06-25 12:42:40 +0600

    login service complete

--- asa/src/components/Login.jsx
+++ asa/src/components/Login.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import vectorImage from "../assets/img/vector.jpg";
+import { useNavigate } from "react-router-dom";
 import logoImage from "../assets/img/rma-logo-white.png";
 import "../assets/css/main.css";
+import AuthServices from "./services/AuthServices";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await AuthServices.login(username, password);
+      
+      if (response.status === 200) {
+        navigate("/dashboard");
+      } 
+      else {
+        setError("Internal Server Issue")
+      }
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
   return (
     <div className="vh-100">
       <div className="container-fluid">
@@ -31,13 +54,15 @@ const Login = () => {
               <p className="customheading">Login to your account</p>
             </div>
             <div className="d-flex flex-column align-items-center justify-content-center h-custom-2 px-5 ms-xl-4">
-              <form style={{ maxWidth: "23rem", width: "100%" }}>
+              <form style={{ maxWidth: "23rem", width: "100%" }} onSubmit={handleLogin}>
                 <div className="form-outline mb-4">
                   <label className="form-label customlabel">Employee ID</label>
                   <input
                     type="text"
-                    name="employeeId"
+                    name="username"
                     className="form-control"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                   />
                 </div>
                 <div className="form-outline mb-4">
@@ -46,16 +71,17 @@ const Login = () => {
                     type="password"
                     name="password"
                     className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
                 <div className="form-outline mb-4">
-                  <Link
-                    to="/dashboard"
+                  <button
                     type="submit"
                     className="btn custombutton w-100"
                   >
                     Login
-                  </Link>
+                  </button>
                 </div>
               </form>
               <div style={{ maxWidth: "23rem", width: "100%" }}>
---
+++ asa/src/components/services/AuthServices.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_APP_BACKEND_URL;
+
+const login = async (username, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/users/sign_in`, {
+      user: {
+        login: username,
+        password: password,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const logout = async () => {
+    try {
+        const response = await axios.delete(`${API_URL}/users/sign_out`);
+        return response;
+      } catch (error) {
+        throw error;
+      }
+  };
+
+export default {
+  login,
+  logout
+};
