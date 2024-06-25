dc50b3e5f2384bef942698270e97621f2a89e9be
PrimeRin <primedruk312@gmail.com>
2024-06-25 12:28:06 +0600

    add more notif

--- asa/src/components/general/Notifications.jsx
+++ asa/src/components/general/Notifications.jsx
@@ -37,6 +37,22 @@ const Notifications = ({ toastRef, profileImage }) => {
             <small className="text-primary">3 minutes ago</small>
           </div>
         </div>
+
+        <div className="d-flex align-items-center">
+          <img
+            src={profileImage}
+            className="rounded-circle me-2"
+            style={{ width: "6vh", height: "6vh" }}
+            alt="Profile"
+          />
+          <div className="ms-3">
+            <p className="textsubheading">
+              Thinley Yoezer requested for salary advance. Click here to see
+              more.
+            </p>
+            <small className="text-primary">5 minutes ago</small>
+          </div>
+        </div>
       </div>
     </div>
   );
