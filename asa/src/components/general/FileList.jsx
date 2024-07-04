043d5cd0f39f8de334602f772f65bd79b56dbdb3
Thinley Yoezer <thinleyyoezer33@gmail.com>
2024-07-04 17:25:25 +0600

    New Changes

--- asa/src/components/general/FileList.jsx
+++ asa/src/components/general/FileList.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import { FaFileAlt, FaEye, FaDownload } from 'react-icons/fa'; // Importing icons
+import React from "react";
+import { FaRegFilePdf } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { IoMdDownload } from "react-icons/io";
 
 const FileList = () => {
   const files = [
     {
-      name: 'document1.pdf',
-      size: '500 KB',
-      preview: () => console.log('Preview document1'),
-      download: () => console.log('Download document1')
+      name: "document1.pdf",
+      size: "500 KB",
+      preview: () => console.log("Preview document1"),
+      download: () => console.log("Download document1"),
     },
     {
-      name: 'image1.jpg',
-      size: '300 KB',
-      preview: () => console.log('Preview image1'),
-      download: () => console.log('Download image1')
+      name: "image1.jpg",
+      size: "300 KB",
+      preview: () => console.log("Preview image1"),
+      download: () => console.log("Download image1"),
     },
   ];
 
-  return (<div>
-    here...
-  </div>
+  return (
+    <div className="file-names pt-2 ps-2 pe-2">
+      <div className="d-flex pb-2">
+        <FaRegFilePdf fontSize={18} color="red" />
+        <p className="m-0 ms-2">testing_file.pdf</p>
+        <div>
+          <button
+            type="button"
+            className="btn btn-warning preview-btn p-0 ms-2"
+          >
+            <FaEye fontSize={12} />{" "}
+            <span style={{ fontSize: "12px" }}>Preview</span>
+          </button>
+          <button type="button" className="btn btn-info preview-btn p-0 ms-2">
+            <IoMdDownload fontSize={12} />{" "}
+            <span style={{ fontSize: "12px" }}>Download</span>
+          </button>
+        </div>
+      </div>
+      <div className="d-flex pb-2">
+        <FaRegFilePdf fontSize={18} color="red" />
+        <p className="m-0 ms-2">testing_file.pdf</p>
+        <div>
+          <button
+            type="button"
+            className="btn btn-warning preview-btn p-0 ms-2"
+          >
+            <FaEye fontSize={12} />{" "}
+            <span style={{ fontSize: "12px" }}>Preview</span>
+          </button>
+          <button type="button" className="btn btn-info preview-btn p-0 ms-2">
+            <IoMdDownload fontSize={12} />{" "}
+            <span style={{ fontSize: "12px" }}>Download</span>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
