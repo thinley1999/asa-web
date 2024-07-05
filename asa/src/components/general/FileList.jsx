34b46131660a9abef28188b9be46e1c35b6c210e
PrimeRin <primedruk312@gmail.com>
2024-07-05 11:29:25 +0600

    update view file

--- asa/src/components/general/FileList.jsx
+++ asa/src/components/general/FileList.jsx
@@ -1,60 +1,47 @@
 import React from "react";
-import { FaRegFilePdf } from "react-icons/fa";
-import { FaEye } from "react-icons/fa";
+import { FaRegFilePdf, FaEye } from "react-icons/fa";
 import { IoMdDownload } from "react-icons/io";
 
-const FileList = () => {
-  const files = [
-    {
-      name: "document1.pdf",
-      size: "500 KB",
-      preview: () => console.log("Preview document1"),
-      download: () => console.log("Download document1"),
-    },
-    {
-      name: "image1.jpg",
-      size: "300 KB",
-      preview: () => console.log("Preview image1"),
-      download: () => console.log("Download image1"),
-    },
-  ];
+const FileList = ({ files }) => {
+  const handlePreview = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
+  const handleDownload = (url, name) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = name;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <div className="file-names pt-2 ps-2 pe-2">
-      <div className="d-flex pb-2">
-        <FaRegFilePdf fontSize={18} color="red" />
-        <p className="m-0 ms-2">testing_file.pdf</p>
-        <div>
-          <button
-            type="button"
-            className="btn btn-warning preview-btn p-0 ms-2"
-          >
-            <FaEye fontSize={12} />{" "}
-            <span style={{ fontSize: "12px" }}>Preview</span>
-          </button>
-          <button type="button" className="btn btn-info preview-btn p-0 ms-2">
-            <IoMdDownload fontSize={12} />{" "}
-            <span style={{ fontSize: "12px" }}>Download</span>
-          </button>
+      {files.map((file, index) => (
+        <div key={index} className="d-flex pb-2 align-items-center">
+          <FaRegFilePdf fontSize={18} color="red" />
+          <p className="m-0 ms-2">{file.name}</p>
+          <div className="ms-auto">
+            <button
+              type="button"
+              className="btn btn-warning preview-btn p-0 ms-2"
+              onClick={() => handlePreview(file.url)}
+            >
+              <FaEye fontSize={12} />{" "}
+              <span style={{ fontSize: "12px" }}>Preview</span>
+            </button>
+            <button
+              type="button"
+              className="btn btn-info preview-btn p-0 ms-2"
+              onClick={() => handleDownload(file.url, file.name)}
+            >
+              <IoMdDownload fontSize={12} />{" "}
+              <span style={{ fontSize: "12px" }}>Download</span>
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="d-flex pb-2">
-        <FaRegFilePdf fontSize={18} color="red" />
-        <p className="m-0 ms-2">testing_file.pdf</p>
-        <div>
-          <button
-            type="button"
-            className="btn btn-warning preview-btn p-0 ms-2"
-          >
-            <FaEye fontSize={12} />{" "}
-            <span style={{ fontSize: "12px" }}>Preview</span>
-          </button>
-          <button type="button" className="btn btn-info preview-btn p-0 ms-2">
-            <IoMdDownload fontSize={12} />{" "}
-            <span style={{ fontSize: "12px" }}>Download</span>
-          </button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
