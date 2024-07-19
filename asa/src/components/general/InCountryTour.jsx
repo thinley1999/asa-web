bb81a37d8b00f923b21e4cd8e713beb33b139c93
PrimeRin <primedruk312@gmail.com>
2024-07-19 14:10:22 +0600

    update advance details

--- asa/src/components/general/InCountryTour.jsx
+++ asa/src/components/general/InCountryTour.jsx
@@ -160,7 +160,7 @@ const InCountryTour = ({ data, showButtons, handleDialogOpen }) => {
 
   const fetchUserDetails = async () => {
     try {
-      const response = await UserServices.showDetail();
+      const response = await UserServices.showDetail(data ? data.user.id : null);
       if (response && response.status === 200) {
         setUser(response.data);
         updateFormDataWithUserName(response.data);
