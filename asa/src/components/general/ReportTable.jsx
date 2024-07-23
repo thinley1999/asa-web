7fad1951e7a4bc7743661339667715304ca88b09
Thinley Yoezer <thinleyyoezer33@gmail.com>
2024-07-23 14:55:25 +0600

    Reports Update

--- asa/src/components/general/ReportTable.jsx
+++ asa/src/components/general/ReportTable.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import DataTable from "react-data-table-component";
-import DataTableExtensions from "react-data-table-component-extensions";
 
 const ReportTable = () => {
   const customStyles = {
